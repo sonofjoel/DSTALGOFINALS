91ebc508563163e377aeedc4211afb57c4f685e5
Admin <Admin@DESKTOP-S0VQ42L>
2025-08-14 00:41:52 +0800

    Add project files.

---
+++ DSTALGOPROJ1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DSTALGOPROJ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("-------Welcome to Student Enrollment System (S.E.S)-------");
+
+            Console.Write("1: View Subject Schedule");
+            Console.Write("2: View Student Schedule");
+            Console.Write("3: Add Subjects to Schedule");
+            Console.Write("4: Change Year Term and ID ");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ DSTALGOPROJ1/UADT.cs
@@ -0,0 +1,65 @@
+﻿namespace DSTALGOPROJ1
+{
+    class UADT
+    {
+        private int top;
+        private object[] array;
+
+        public UADT()
+        {
+            int top = -1;
+            array = new object[5];
+        }
+
+        public void Push(object item)
+        {
+            if (top < array.Length - 1)
+            {
+                top++;
+                array[top] = item;
+            }
+            else throw new Exception("Stack Overflow...");
+        }
+
+        public object Pop()
+        {
+            if (top > -1)
+            {
+                object item = array[top];
+                top--;
+                return item;
+            }
+            else throw new Exception("Stack Underflow");
+        }
+
+        public object Peek()
+        {
+            if (top > -1)
+            {
+                object item = array[top];
+
+                return item;
+            }
+            else throw new Exception("Stack Underflow");
+        }
+
+        public void clear()
+        {
+            top = -1;
+        }
+
+        public void PrintStack()
+        {
+            for (int i = top; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+
+
+
+
+
+    }
+}
